fbaa6bd192877fbf3a1e9dea9960ce9169e53edc
Filip Strycko <filipstrycko@Filips-MacBook-Pro.local>
2013-02-14 12:53:50 +0100

    updating README and guide to create admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
          #:recoverable, :rememberable,
-          :trackable, :validatable
+          :trackable, :validatable            , :registerable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
